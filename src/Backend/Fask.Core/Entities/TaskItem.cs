ca119f2884d4f4f5321ca56607eeaf906a04bfcf
Przemyslaw Sapiecha <sapiech@live.com>
2024-03-19 22:59:34 +0100

    Planning in progress

---
+++ src/Backend/Fask.Core/Entities/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using Fask.Core.ValueObjects;
+using Fask.Shared.Abstraction.Core;
+
+namespace Fask.Core.Entities;
+
+public sealed class TaskItem : AggregateRoot<Guid>
+{
+    private TaskName _name;
+    private string _description;
+    private TaskEffort _effort;
+    
+    private readonly IList<>
+}
---
+++ src/Backend/Fask.Core/ValueObjects/TaskEffort.cs
@@ -0,0 +1,19 @@
+﻿namespace Fask.Core.ValueObjects;
+
+public record TaskEffort
+{
+    public ushort Value { get; }
+
+    public TaskEffort(ushort value)
+    {
+        if (value == 0)
+        {
+            throw new ArgumentException(value.ToString());
+        }
+        
+        Value = value;
+    }
+
+    public static implicit operator ushort(TaskEffort effort) => effort.Value;
+    public static implicit operator TaskEffort(ushort name) => new(name);
+}
